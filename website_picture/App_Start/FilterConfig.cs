65f79998a462bfdd4077d549ef9c9f6a00c3d744
tuongboss1903 <64318082+tuongboss1903@users.noreply.github.com>
2021-03-29 10:22:10 +0700

    tuong test

---
+++ website_picture/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace website_picture
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ website_picture/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="website_picture.MvcApplication" Language="C#" %>
